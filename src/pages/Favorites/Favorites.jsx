827b27e7593ef1d1128f87b0fd6a7482fc4bf042
Jorge Ossa <jorgehossa@gmail.com>
2020-12-09 02:32:30 -0500

    📦 NEW: Add Favorites page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { MdKeyboardBackspace } from 'react-icons/md';
+import { Container, BackButton } from './Favorites.styles';
 //import PropTypes from 'prop-types';
 
 const Favorites = () => (
-  <div className="FavoritesWrapper">
-    Test content
-  </div>
+  <Container>
+    <BackButton to="/"> <MdKeyboardBackspace/> Back</BackButton>
+  </Container>
 );
 
 // Favorites.propTypes = {
--- src/pages/Favorites/Favorites.styles.js
+++ src/pages/Favorites/Favorites.styles.js
@@ -1,6 +1,14 @@
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-// export const Test = styled.div`
-//  display: flex;
-// `;
-//
+export const Container = styled.div`
+  display: block;
+`;
+
+export const BackButton = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: grey;
+  padding: .5rem 0;
+`
