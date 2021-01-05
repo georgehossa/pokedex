dd2532d5094d006f7b4995d2c5feef6d99c8c7d7
Jorge Ossa <jorgehossa@gmail.com>
2021-01-05 01:09:26 -0500

    📦 NEW: Add Favorites styles

--- src/pages/Favorites/Favorites.styles.js
+++ src/pages/Favorites/Favorites.styles.js
@@ -3,8 +3,14 @@ import { Link } from 'react-router-dom';
 import { MdKeyboardBackspace } from 'react-icons/md';
 
 export const Container = styled.div`
-  display: block;
-`;
+  display: grid;
+  grid-template: auto / 1fr;
+  grid-gap: 1rem;
+  padding: 1rem;
+  @media (min-width: 1440px) {
+    grid-template: auto / repeat(auto-fit, 300px);
+  }
+`
 
 export const BackButton = styled(Link)`
   display: flex;
