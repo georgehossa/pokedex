9ea9c0229f17f970c78c0b6657004510737c480c
Jorge Ossa <jorgehossa@gmail.com>
2021-01-23 01:21:00 -0500

    📦 NEW: Add search sugestion

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,10 +11,24 @@ const Home = ({ myFavorites }) => {
   const [inputPokemon, setInputPokemon] = useState('');
   const [resultPokemon, setResultPokemon] = useState({});
   const [allPokemon, setAllPokemon] = useState([]);
+  const [suggestedPokemon, setSuggestedPokemon] = useState([]);
   const URL = 'https://pokeapi.co/api/v2/pokemon/';
 
   const handleChange = (e) => {
+      if(e.target.value) {
+        setSuggestedPokemon(allPokemon.filter(pokemon => pokemon.toLowerCase().includes(e.target.value)));
+        console.log(suggestedPokemon);
+      } else {
+        setSuggestedPokemon([]);
+      }
       setInputPokemon(e.target.value);
+  }
+
+  const setValue = (e, ref) => {
+    let pokemon = e.target.innerText;
+    ref.current.value = pokemon;
+    setInputPokemon(ref.current.value);
+    setSuggestedPokemon([]);
   }
 
   const handleClick = () => {
@@ -35,7 +49,12 @@ const Home = ({ myFavorites }) => {
   }
 
   useEffect(() => {
-    axios.get(`${URL}?limit=2000`).then(res => setAllPokemon(res.data.results.map(pokemon => pokemon.name)));
+    axios.get(`${URL}?limit=2000`)
+      .then(res => {
+        const allPokemonCount = res.data.count;
+        axios.get(`${URL}?limit=${allPokemonCount}`)
+          .then(response => setAllPokemon(response.data.results.map(pokemon => pokemon.name)));
+      });
     return () => {
       null
     }
@@ -43,7 +62,7 @@ const Home = ({ myFavorites }) => {
 
   return (
       <Container>
-        <SearchBar onChange={handleChange} onClick={handleClick} suggestionData={allPokemon} />
+        <SearchBar onChange={handleChange} onClick={handleClick} setValue={setValue} suggestedPokemon={suggestedPokemon} />
         <CardContainer>
           {
             Object.keys(resultPokemon).length !== 0 ? <PokemonCard id={resultPokemon.id} name={resultPokemon.name} type={resultPokemon.type} image={resultPokemon.image} isFavorite={checkFavorite(myFavorites, resultPokemon.id)}/> : null
